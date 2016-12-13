5a53cd8f233029f8662263b52fa42f882ebb01b3
Oleksandr Sribnyi <e147048@student.tuwien.ac.at>
2016-12-13 12:47:02 +0100

    Added telephone nr entry to user profile

--- view/js/profile.js
+++ view/js/profile.js
@@ -1,6 +1,7 @@
 if (sessionStorage.loggedInUserName == undefined) {
 	sessionStorage.loggedInUserName = "Horst Peter";
 	sessionStorage.loggedInUserMatNr = "&nbsp;&nbsp;&nbsp;1231234";
+	sessionStorage.loggedInUserTelNr = "+436991234567";
 	sessionStorage.loggedInUserMail = "horst@peter.de";
 	sessionStorage.loggedInUserAdd = "Ich bin der tollste, beste, sonst alles überhaupt!";
 }
@@ -9,9 +10,11 @@ document.getElementById("loggedInUserName").value = sessionStorage.loggedInUserN
 document.getElementById("loggedInUserMatNr").innerHTML = sessionStorage.loggedInUserMatNr;
 document.getElementById("loggedInUserMail").value = sessionStorage.loggedInUserMail;
 document.getElementById("loggedInUserAdd").value = sessionStorage.loggedInUserAdd;
+document.getElementById("loggedInUserTelNr").value = sessionStorage.loggedInUserTelNr;
 
 function saveLoggedInUserData() {
 	sessionStorage.loggedInUserName = document.getElementById("loggedInUserName").value;
 	sessionStorage.loggedInUserMail = document.getElementById("loggedInUserMail").value;
+	sessionStorage.loggedInUserTelNr = document.getElementById("loggedInUserTelNr").value;
 	sessionStorage.loggedInUserAdd = document.getElementById("loggedInUserAdd").value;
 }
