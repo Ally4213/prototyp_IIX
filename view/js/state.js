d83f1e442ccf48b3decb85dcbb72dfa0ff1e15ce
Andreas Klebinger <klebinger.andreas@gmx.at>
2016-12-13 15:43:24 +0100

    Login toggle via Cookie

---
+++ view/js/state.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Andi on 13.12.2016.
+ */
+
+login = function() {
+    Cookies.set('state', 'logged-in');
+
+    var lo = $(".logged-out");
+    lo.children().hide();
+    lo.hide();
+
+    var li = $(".logged-in");
+    li.children().show();
+    li.show();
+
+
+};
+
+logout = function () {
+    Cookies.remove('state');
+
+    var lo = $(".logged-out");
+    lo.children().show();
+    lo.show();
+
+    var li = $(".logged-in");
+    li.children().hide();
+    li.hide();
+}
