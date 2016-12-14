f2e9b13f878e90d15bc84fe0bdd0d17cab356ed4
Tobias Geibinger <tgeibinger@gmail.com>
2016-12-14 16:31:00 +0100

    changes to details and upload

--- view/js/create.js
+++ view/js/create.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$('document').ready(function(){
     $('select').on('change', function() {
   		if(this.value == 'written'){
   			$('#author-section').hide();
@@ -7,4 +7,19 @@ $(document).ready(function(){
   		}
 	});
 	$('select').val('script');
+	$('#image-chooser').change(function() {
+		var file = this.files[0];
+		
+		var reader = new FileReader();
+
+  		reader.onload = function(event) {
+    		the_url = event.target.result
+    		$('#image').attr('src', the_url);
+  		}
+ 
+  		reader.readAsDataURL(file);
+	});
+	$('#backButton').click(function(){
+   		window.location.href='./landing.html';
+	})
 }); 
